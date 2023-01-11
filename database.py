67db04eb7a1819d949b17a39f3d74ff108dace13
kimich <ickim1218@gmail.com>
2023-01-11 14:45:07 +0900

    fix: 폴더 삭제 및 생성

--- database.py
+++ database.py
@@ -1,12 +1,37 @@
+# database.py
+# database 연결과 관련된 파일
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/bomastick" #로컬로 설정했을 경우 본인의 컴퓨터에 맞게 1234는 mysql비밀번호 bomastick은 데이터베이스 이름
+from dotenv import load_dotenv
 
 
 
+import os
+
+# 환경변수 로드
+load_dotenv()
+
+# DB 주소
+DB_URL = (f"mysql+pymysql://{os.getenv('MYSQL_USER')}" +
+          f":{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}" +
+          f":{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DATABASE')}?charset=utf8mb4")
+
+
+# sqlalchemy 엔진, main.py에서 사용
+engine = create_engine(DB_URL, encoding = 'utf8')
+
+# 데이터베이스 세션클래스, 이를 이용해 생성한 인스턴스로 DB에 접근해서 CRUD가능
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# DB모델이나 클래스를 만들기 위해 선언한 클래스(후에 상속해서 사용)
 Base = declarative_base()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
