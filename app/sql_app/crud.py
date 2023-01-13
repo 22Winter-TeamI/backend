0a5937483f7d3679782041c8545cac84332d142a
수연 <syeon0131@naver.com>
2023-01-13 17:23:41 +0900

    feat:갤러리 api & db 수정

--- app/sql_app/crud.py
+++ app/sql_app/crud.py
@@ -1,43 +1,36 @@
 from sqlalchemy.orm import Session
 
 from . import models, schemas
+from sqlalchemy import and_
 
-
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.user_id == user_id).first()
-
-
+#is_deleted라면 없는 이름이라고 뜨기
 def get_user_by_name(db: Session, name: str):
     return db.query(models.User).filter(models.User.name == name).first()
 
-
-# def get_users(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.User).offset(skip).limit(limit).all()
-
-
-def create_user(db: Session, user: schemas.User):
-    # fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(name=user.name)#, hashed_password=fake_hashed_password)
+def create_user(db: Session, user: str):
+   
+    db_user = models.User(name=user.name)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+ 
+def get_user_name_from_user_id(db: Session, user_name: str):
+    a= db.query(models.User.user_id).filter(models.User.name == user_name).first()
+    b=int(str(a).strip('(').strip(')').strip(','))
+    return b
 
-def get_photos(db: Session, name: str):
-    user_id=db.query(models.User).filter(models.User.name == name).first()
-    return db.query(models.ResultPhoto).filter(models.ResultPhoto.user_id == name).first()
-    #, hashed_password=fake_hashed_password)
+def get_user_images(db: Session, user_id: str):
+    photo_ids= db.query(models.UploadedPhoto.result_name,models.UploadedPhoto.photo_name).filter(models.UploadedPhoto.user_id == user_id).all()
+    
+    return photo_ids
 
+def get_user_images_rmbackgournd(db: Session, user_id: str):
+    photo_ids= db.query(models.UploadedPhoto.result_name,models.UploadedPhoto.photo_name).filter(and_(models.UploadedPhoto.user_id == user_id,models.UploadedPhoto.update_type== "REMOVEBACKGROUND")).all()
 
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
+    return photo_ids
 
+def get_user_images_paint(db: Session, user_id: str):
+    photo_ids= db.query(models.UploadedPhoto.result_name,models.UploadedPhoto.photo_name).filter(and_(models.UploadedPhoto.user_id == user_id,models.UploadedPhoto.update_type== "CHANGESTYLE")).all()
 
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
-
-
+    return photo_ids
--- main.py
+++ main.py
@@ -38,30 +38,3 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
-# @app.get("/users/", response_model=list[schemas.User])
-# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     users = crud.get_users(db, skip=skip, limit=limit)
-#     return users
-
-
-# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
-# def create_item_for_user(
-#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-# ):
-#     return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-# @app.get("/items/", response_model=list[schemas.Item])
-# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     items = crud.get_items(db, skip=skip, limit=limit)
-#     return items
-
-# @app.get("/images")
-# def gallery_all_user(user_name: str, db: Session = Depends(get_db)):
-#     get_photos = crud.get_photos(db, user_name=user_name)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-    
-#     return db_user
-
-
