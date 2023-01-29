c7697db76c5dd58938d3eb395e2d5e5925eb4a5f
kimich <ickim1218@gmail.com>
2023-01-29 23:21:05 +0900

    fix: load 값 requestbody 요청으로 변경

--- endpoint/aws_router.py
+++ endpoint/aws_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile, Depends, HTTPException
+from fastapi import APIRouter, File, UploadFile, Depends, HTTPException, Form
 from transparent_background import Remover
 
 from app.aws.bucket import *
@@ -31,19 +31,24 @@ def get_db():
 
 @router.post("/load/")
 #첫번째 인자가 기본 사진 두번째 인자가 배경 사진 
-async def load_photo(file:UploadFile=File(...),file2:UploadFile=File(...), file3:UploadFile=File(...), type :Optional[str]=None, userId :Optional[int]=None, db: Session=Depends(get_db)):
+# async def load_photo(loadInfo: schemas.Load, file:UploadFile=File(...),file2:UploadFile=File(...), file3:UploadFile=File(...), type :Optional[str]=None, userId :Optional[int]=None, db: Session=Depends(get_db)):
+async def load_photo(file:UploadFile=File(...), file2:UploadFile=File(...), file3:UploadFile=File(...), userName: str =Form(...), choiceType: str =Form(...),db: Session=Depends(get_db)):
+    
+    userId=crud.get_user_name_from_user_id(db,user_name=userName)
+    
     filename=f"{uuid.uuid4()}.jpeg"
     resultfilename=f"{uuid.uuid4()}.jpeg"
-    if(type=="CHANGESTYLE"):
+    
+    if(choiceType=="CHANGESTYLE"):
        content= changeStyle(file)
        post_bucket(content,resultfilename) 
-       photo=models.Photo(user_id=userId,photo_name=filename,update_type=type, result_name=resultfilename)
+       photo=models.Photo(user_id=userId, photo_name=filename, update_type=choiceType, result_name=resultfilename)
        
 
-    elif(type=="REMOVEBACKGROUND"):
+    elif(choiceType=="REMOVEBACKGROUND"):
         content= changeBackground(file,file2)
         post_bucket(content,resultfilename) 
-        photo=models.Photo(user_id=userId,photo_name=filename,update_type=type, result_name=resultfilename)
+        photo=models.Photo(user_id=userId, photo_name=filename, update_type=choiceType, result_name=resultfilename)
         
     
     crud.create_images(db=db,image=photo)
@@ -59,12 +64,13 @@ async def load_photo(file:UploadFile=File(...),file2:UploadFile=File(...), file3
     return {"resultfilename":resultfilename}
 
 
+
 @router.get("/download/")
 async def download_photo(db: Session = Depends(get_db), userId: Optional[int]=None, photoId: Optional[int]=None):
     image=crud.get_photo(db=db, user_id=userId, photo_id=photoId)
     pull_bucket(image[0])
     return True
-   
+
 
 
 def changeBackground(img1: UploadFile=File(...), img2: UploadFile=File(...)):
